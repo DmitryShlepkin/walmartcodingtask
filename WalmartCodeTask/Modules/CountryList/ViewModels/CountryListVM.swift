ac8e131da98aa518cba6ba494c15873abb4ef891
Dmitry Shlepkin <acidl33t@gmail.com>
2025-03-13 08:37:28 -0400

    URL update

--- WalmartCodeTask/Modules/CountryList/ViewModels/CountryListVM.swift
+++ WalmartCodeTask/Modules/CountryList/ViewModels/CountryListVM.swift
@@ -17,7 +17,7 @@ protocol CountryListViewModeling {
 final class CountryListVM: CountryListViewModeling {
     
     enum EndPoints: String {
-        case countries = "https://gist.githubusercontent.com/peymano-wmt/32dcb892b06648910ddd40406e37fdab/raw/db25946fd77c5873b0303b858e861ce724e0dcd0/countries.jsonn"
+        case countries = "https://gist.githubusercontent.com/peymano-wmt/32dcb892b06648910ddd40406e37fdab/raw/db25946fd77c5873b0303b858e861ce724e0dcd0/countries.json"
     }
     
     private var networkManager: NetworkManaging
